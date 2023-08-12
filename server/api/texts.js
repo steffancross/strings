dc44ab8f19834fa979fc1940c33fbdca51790fe8
Steffan Cross <steffancross@berkeley.edu>
2023-08-12 12:43:26 -0700

    added get route for getting text by tag and getting text by content

--- server/api/texts.js
+++ server/api/texts.js
@@ -1,14 +1,61 @@
 const router = require("express").Router();
 module.exports = router;
+const { Op } = require("sequelize");
 const { models } = require("../db");
 const { Text, Tag } = models;
 
+// all user text
 router.get("/", async (req, res, next) => {
   try {
     const { userId } = req.query;
     const texts = await Text.findAll({
       where: {
         userId: userId,
+      },
+      attributes: ["content", "id"],
+      include: {
+        model: Tag,
+        attributes: ["id", "name"],
+        through: { attributes: [] },
+      },
+    });
+    res.send(texts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// finds texts by tag
+router.get("/byTag", async (req, res, next) => {
+  try {
+    const { userId, tagNames } = req.query;
+    const texts = await Text.findAll({
+      where: {
+        userId: userId,
+      },
+      attributes: ["content", "id"],
+      include: {
+        model: Tag,
+        where: { name: tagNames },
+        attributes: ["id", "name"],
+        through: { attributes: [] },
+      },
+    });
+    res.send(texts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/byContent", async (req, res, next) => {
+  try {
+    const { userId, searchTerm } = req.query;
+    const texts = await Text.findAll({
+      where: {
+        userId: userId,
+        content: {
+          [Op.iLike]: `%${searchTerm}%`,
+        },
       },
       attributes: ["content", "id"],
       include: {
