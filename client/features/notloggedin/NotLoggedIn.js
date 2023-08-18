59d53dc58f7a5264f346d5f4ea1ddfce87be90ed
Steffan Cross <steffancross@berkeley.edu>
2023-08-17 21:52:58 -0700

    some styling for notloggedin, begininng of the beast

--- client/features/notloggedin/NotLoggedIn.js
+++ client/features/notloggedin/NotLoggedIn.js
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 const NotLoggedIn = () => {
   return (
     <>
-      <div>
-        <p>Hi, welcome to archive</p>
-        <p>blah blah blah</p>
-        <p>blah blah blah</p>
-      </div>
-      <div>
-        <p>Please login</p>
-        <Link to="/login">Here</Link>
-      </div>
-      <div>
-        <p>Please sign up</p>
-        <Link to="/signup">Here</Link>
+      <div className="centering-div">
+        <div>
+          <p>Hi, welcome to archive</p>
+          <p>blah blah blah</p>
+          <p>blah blah blah</p>
+          <div className="notloggedin">
+            <p>Please login&nbsp;</p>
+            <Link to="/login">here</Link>
+          </div>
+          <div className="notloggedin">
+            <p>Please sign up&nbsp;</p>
+            <Link to="/signup">here</Link>
+          </div>
+        </div>
       </div>
     </>
   );
