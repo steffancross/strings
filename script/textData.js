70fe1e2906b2979afcb6330831bbe0b36731d3db
Steffan Cross <steffancross@berkeley.edu>
2023-11-19 21:44:20 -0800

    updating seed data

--- script/textData.js
+++ script/textData.js
@@ -1,13 +1,53 @@
 const textData = [
   {
-    content: "this is hopeful and inspiring",
+    content:
+      "If they can get you asking the wrong questions, they don't have to worry about answers.",
+    description: "Thomas Pynchon, Gravity's Rainbow",
     userId: 1,
-    tags: ["hope", "inspire"],
+    tags: ["quote"],
   },
   {
-    content: "this is just cool",
+    content: "We judge others by their actions and ourselves by our intentions",
+    description: "Stephen Covey",
     userId: 1,
-    tags: ["cool"],
+    tags: ["quote"],
+  },
+  {
+    content:
+      "Sometimes you never realize the value of a moment until it becomes a memory",
+    description: "Dr. Seuss",
+    userId: 1,
+    tags: ["quote", "sad"],
+  },
+  {
+    content: "",
+    description: "",
+    userId: 1,
+    tags: [],
+  },
+  {
+    content: "",
+    description: "",
+    userId: 1,
+    tags: [],
+  },
+  {
+    content: "",
+    description: "",
+    userId: 1,
+    tags: [],
+  },
+  {
+    content: "",
+    description: "",
+    userId: 1,
+    tags: [],
+  },
+  {
+    content: "",
+    description: "",
+    userId: 1,
+    tags: [],
   },
 ];
 
