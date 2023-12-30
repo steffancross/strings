81102c8f104eb1d7a238480e0fa055c222bbdde3
Steffan Cross <steffancross@berkeley.edu>
2023-12-30 15:56:34 -0800

    removed slider and replaced with numbers. selected styling now is primary color and unselected is tertiary

--- client/features/styling/Styling.js
+++ client/features/styling/Styling.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ChromePicker } from "react-color";
 
 const Styling = () => {
+  const columnList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const [color, setColor] = useState("#ff0000");
   const [num, setNum] = useState(1);
 
@@ -12,7 +13,7 @@ const Styling = () => {
       document.documentElement.style.setProperty("--primary-color", color);
     } else if (num === 2) {
       document.documentElement.style.setProperty("--secondary-color", color);
-    } else {
+    } else if (num === 3) {
       document.documentElement.style.setProperty("--tertiary-color", color);
     }
   };
@@ -21,9 +22,24 @@ const Styling = () => {
     <>
       <div>
         <div>
-          <button onClick={() => setNum(1)}>Primary Color</button>
-          <button onClick={() => setNum(2)}>Secondary Color</button>
-          <button onClick={() => setNum(3)}>Tertiary Color</button>
+          <button
+            onClick={() => setNum(1)}
+            className={num === 1 ? "selected" : "unselected"}
+          >
+            Primary Color
+          </button>
+          <button
+            onClick={() => setNum(2)}
+            className={num === 2 ? "selected" : "unselected"}
+          >
+            Secondary Color
+          </button>
+          <button
+            onClick={() => setNum(3)}
+            className={num === 3 ? "selected" : "unselected"}
+          >
+            Tertiary Color
+          </button>
         </div>
         <ChromePicker
           disableAlpha={true}
@@ -32,7 +48,15 @@ const Styling = () => {
         />
       </div>
       <div>
-        <p>Columns</p>
+        <button
+          onClick={() => setNum(4)}
+          className={num === 4 ? "selected" : "unselected"}
+        >
+          Columns
+        </button>
+        {columnList.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
       </div>
     </>
   );
