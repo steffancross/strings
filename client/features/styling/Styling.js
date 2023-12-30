73eef6ae801547bc0a588d6cc084f9a98e4218a9
Steffan Cross <steffancross@berkeley.edu>
2023-12-29 18:55:01 -0800

    all three colors can be set. colors update as user changes them

--- client/features/styling/Styling.js
+++ client/features/styling/Styling.js
@@ -4,18 +4,31 @@ import { SketchPicker } from "react-color";
 
 const Styling = () => {
   const [color, setColor] = useState("#ff0000");
+  const [num, setNum] = useState(1);
 
-  const handleColorChange = () => {
-    document.documentElement.style.setProperty("--primary-color", color);
+  const handleColorChange = (num, colorToSet) => {
+    setColor(colorToSet.hex);
+
+    if (num === 1) {
+      document.documentElement.style.setProperty("--primary-color", color);
+    } else if (num === 2) {
+      document.documentElement.style.setProperty("--secondary-color", color);
+    } else {
+      document.documentElement.style.setProperty("--tertiary-color", color);
+    }
   };
 
   return (
     <>
       <div>
+        <div>
+          <button onClick={() => setNum(1)}>Primary Color</button>
+          <button onClick={() => setNum(2)}>Secondary Color</button>
+          <button onClick={() => setNum(3)}>Tertiary Color</button>
+        </div>
         <SketchPicker
           color={color}
-          onChange={(color) => setColor(color.hex)}
-          onChangeComplete={() => handleColorChange()}
+          onChange={(color) => handleColorChange(num, color)}
         />
       </div>
     </>
