4f78e055bf9a13833398a53759941e53dcb45824
Steffan Cross <steffancross@berkeley.edu>
2023-12-30 20:50:30 -0800

    put route in the styling slice. function to set styles

--- client/features/styling/stylingSlice.js
+++ client/features/styling/stylingSlice.js
@@ -1,11 +1,20 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// get all tags
+// get styles
 export const fetchStyles = createAsyncThunk(
   "users/fetch",
   async ({ userId }) => {
     const { data } = await axios.get(`/api/users/styles?userId=${userId}`);
+    return data;
+  }
+);
+
+// update styles
+export const editStyles = createAsyncThunk(
+  "users/put",
+  async ({ primaryColor, secondaryColor, tertiaryColor, columns, userId }) => {
+    const { data } = await axios.put(`/api/users/styles`);
     return data;
   }
 );
--- client/features/utils/HelperFunctions.js
+++ client/features/utils/HelperFunctions.js
@@ -44,3 +44,18 @@ export function graphParser(data) {
 
   return { nodes, links };
 }
+
+export function setStyles(
+  primaryColor,
+  secondaryColor,
+  tertiaryColor,
+  columns
+) {
+  document.documentElement.style.setProperty("--primary-color", primaryColor);
+  document.documentElement.style.setProperty(
+    "--secondary-color",
+    secondaryColor
+  );
+  document.documentElement.style.setProperty("--tertiary-color", tertiaryColor);
+  document.documentElement.style.setProperty("--grid-cols", columns);
+}
