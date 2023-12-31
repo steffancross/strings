7e3ea0ac6a01a4a693adafee063578385e3b686a
Steffan Cross <steffancross@berkeley.edu>
2023-12-31 12:16:21 -0800

    some styling. also make sure the updated values come in

--- client/features/styling/Styling.js
+++ client/features/styling/Styling.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { ChromePicker } from "react-color";
 import { useSelector, useDispatch } from "react-redux";
-import { editStyles } from "./stylingSlice";
+import { editStyles, fetchStyles } from "./stylingSlice";
 import { motion } from "framer-motion";
 
 const Styling = () => {
   const dispatch = useDispatch();
   const columnList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const [selectedColumn, setSelectedColumn] = useState(columns);
   const [color, setColor] = useState("#000");
   const [styleMode, setStyleMode] = useState(1);
   const userId = useSelector((state) => state.auth.me.id);
@@ -15,6 +14,7 @@ const Styling = () => {
   const secondaryColor = useSelector((state) => state.styles.secondaryColor);
   const tertiaryColor = useSelector((state) => state.styles.tertiaryColor);
   const columns = useSelector((state) => state.styles.columns);
+  const [selectedColumn, setSelectedColumn] = useState(columns);
 
   useEffect(() => {
     updateDbWithStyles();
@@ -23,11 +23,11 @@ const Styling = () => {
   const handleColorChange = (num, colorToSet) => {
     setColor(colorToSet.hex);
 
-    if (styleMode === 1) {
+    if (num === 1) {
       document.documentElement.style.setProperty("--primary-color", color);
-    } else if (styleMode === 2) {
+    } else if (num === 2) {
       document.documentElement.style.setProperty("--secondary-color", color);
-    } else if (styleMode === 3) {
+    } else if (num === 3) {
       document.documentElement.style.setProperty("--tertiary-color", color);
     }
   };
@@ -41,7 +41,9 @@ const Styling = () => {
       userId,
     };
 
-    dispatch(editStyles(styleUpdate));
+    dispatch(editStyles(styleUpdate)).then(() => {
+      dispatch(fetchStyles({ userId }));
+    });
   };
 
   return (
@@ -53,53 +55,63 @@ const Styling = () => {
         exit={{ opacity: 0 }}
         transition={{ duration: 0.3 }}
       >
-        <div>
-          <button
-            onClick={() => setStyleMode(1)}
-            className={styleMode === 1 ? "selected-color" : "unselected-color"}
-          >
-            Primary Color
-          </button>
-          <button
-            onClick={() => setStyleMode(2)}
-            className={styleMode === 2 ? "selected-color" : "unselected-color"}
-          >
-            Secondary Color
-          </button>
-          <button
-            onClick={() => setStyleMode(3)}
-            className={styleMode === 3 ? "selected-color" : "unselected-color"}
-          >
-            Tertiary Color
-          </button>
-          <button
-            onClick={() => setStyleMode(4)}
-            className={styleMode === 4 ? "selected-color" : "unselected-color"}
-          >
-            Columns
-          </button>
-        </div>
-        <div>
-          {styleMode === 4 ? (
-            <div id="columns">
-              {columnList.map((item, index) => (
-                <p
-                  key={index}
-                  className={selectedColumn === item ? "selected-column" : ""}
-                  onClick={() => setSelectedColumn(item)}
-                >
-                  {item}
-                </p>
-              ))}
-            </div>
-          ) : (
-            <ChromePicker
-              disableAlpha={true}
-              color={color}
-              onChange={(color) => handleColorChange(styleMode, color)}
-              onChangeComplete={() => updateDbWithStyles()}
-            />
-          )}
+        <div className="single-mattag">
+          <div className="style-selector">
+            <button
+              onClick={() => setStyleMode(1)}
+              className={
+                styleMode === 1 ? "selected-color" : "unselected-color"
+              }
+            >
+              Primary Color
+            </button>
+            <button
+              onClick={() => setStyleMode(2)}
+              className={
+                styleMode === 2 ? "selected-color" : "unselected-color"
+              }
+            >
+              Secondary Color
+            </button>
+            <button
+              onClick={() => setStyleMode(3)}
+              className={
+                styleMode === 3 ? "selected-color" : "unselected-color"
+              }
+            >
+              Tertiary Color
+            </button>
+            <button
+              onClick={() => setStyleMode(4)}
+              className={
+                styleMode === 4 ? "selected-color" : "unselected-color"
+              }
+            >
+              Columns
+            </button>
+          </div>
+          <div className="style-mode">
+            {styleMode === 4 ? (
+              <div id="columns">
+                {columnList.map((item, index) => (
+                  <p
+                    key={index}
+                    className={selectedColumn === item ? "selected-column" : ""}
+                    onClick={() => setSelectedColumn(item)}
+                  >
+                    {item}
+                  </p>
+                ))}
+              </div>
+            ) : (
+              <ChromePicker
+                disableAlpha={true}
+                color={color}
+                onChange={(color) => handleColorChange(styleMode, color)}
+                onChangeComplete={() => updateDbWithStyles()}
+              />
+            )}
+          </div>
         </div>
       </motion.div>
     </>
