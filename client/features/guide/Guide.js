5b576997fb2f983a73b0cd80e7f74e09a9b7119d
Steffan Cross <steffancross@berkeley.edu>
2024-07-14 18:01:28 -0700

    reworded guide section

--- client/features/guide/Guide.js
+++ client/features/guide/Guide.js
@@ -1,18 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import "../../styles/guide.scss";
 
 const Guide = () => {
-  const [currIndex, setCurrIndex] = useState(0);
-
-  const next = () => {
-    setCurrIndex((currIndex) => currIndex + 1);
-  };
-
-  const prev = () => {
-    setCurrIndex((currIndex) => currIndex - 1);
-  };
-
   return (
     <>
       <motion.div
@@ -23,69 +13,75 @@ const Guide = () => {
         transition={{ duration: 1.0, delay: 0.5 }}
       >
         <div className="guide-item">
-          <h1 className="guide-title">Search Bar</h1>
+          <h1 className="guide-title">Command Bar</h1>
           <p>
-            The search bar at the top of the window is the main way to navigate
-            the site. Besides when a popup is open, pressing <em>'/'</em> will
-            focus the search bar and allow you to type commands.
+            The command bar at the top of the window is the main way to interact
+            with the site. From it, both navigation and functional commands can
+            be executed. All commands can be executed from any page and will
+            navigate automatically to the relevant one. Besides when a modal is
+            open, pressing <em>'/'</em> will focus the search bar and allow you
+            to type commands.
           </p>
+        </div>
+        <div className="guide-item">
           <h1 className="guide-title">Navigation</h1>
-          <p>
-            With navigation commands, just type location you want into the
-            search bar at the top and you'll be taken to the corresponding page.
-          </p>
+          <p>Navigation commands will take you to the corresponding page.</p>
           <div className="guide-command">
             <em>home</em>
-            <p>&nbsp;goes to home</p>
+            <p>&nbsp;to home</p>
           </div>
           <div className="guide-command">
             <em>guide</em>
-            <p>&nbsp;goes to the guide</p>
+            <p>&nbsp;to guide</p>
           </div>
           <div className="guide-command">
             <em>graph</em>
-            <p>&nbsp;goes to the graph</p>
+            <p>&nbsp;to graph</p>
           </div>
           <div className="guide-command">
             <em>tags</em>
-            <p>&nbsp;goes to all tags</p>
+            <p>&nbsp;to all tags</p>
           </div>
           <div className="guide-command">
             <em>about</em>
-            <p>&nbsp;goes to about</p>
+            <p>&nbsp; to about</p>
+          </div>
+          <div className="guide-command">
+            <em>style</em>
+            <p>&nbsp; to style</p>
           </div>
         </div>
         <div className="guide-item">
           <h1 className="guide-title">Mat</h1>
           <p>
-            With the 'search' and 'filter' commands, there is a bit of syntax.
-            The base command, followed by a <em>':'</em> and then your search
-            criteria.
+            With the <em>search</em> and <em>filter</em> commands, there is a
+            bit of syntax. The base command, followed by a <em>':'</em> and then
+            your search criteria.
           </p>
           <div className="guide-command">
             <em>new</em>
-            <p>&nbsp;opens the menu to create a new mat</p>
+            <p>&nbsp;opens the modal to create a new mat</p>
           </div>
           <div className="guide-command">
             <em>search</em>
             <p>
-              &nbsp;this will search all your mats and return the ones that
-              contain your search query at any location
+              &nbsp;will search all your mats and return the ones that contain
+              your search query at any location
             </p>
           </div>
           <div className="guide-example">
             <small>example</small>
             <em>search: and</em>
             <p>
-              This would return all mats that contain 'and' whether it's a word
-              on its own or part of a word such as 'brand'
+              This would return all mats that contain <em>and</em> whether it's
+              a word on its own, or part of a word such as 'brand'
             </p>
           </div>
           <div className="guide-command">
             <em>filter</em>
             <p>
-              &nbsp;this functions like search but with tags and will return all
-              mats associated with the input tag
+              &nbsp;functions like search but with tags and will return all mats
+              associated with the input tag
             </p>
           </div>
           <div className="guide-example">
@@ -97,9 +93,9 @@ const Guide = () => {
         <div className="guide-item">
           <h1 className="guide-title">Tags</h1>
           <p>
-            When adding or editing a mat in the 'tags' input field, once again,
-            there is syntax. If adding or assigning more than one tag, separate
-            them by commas.
+            When adding or editing a mat in the 'tags' input field, there is
+            syntax. If adding or assigning more than one tag, separate them by
+            commas.
           </p>
           <div className="guide-example">
             <em>hope, cool</em>
